2cf3ab71a971bdb98c89bbcf62080ff72ac440c8
ivan <ivan.stebler@diveria.com>
2025-03-22 22:08:13 -0300

    Add database integration and update chat message handling

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 
+dotenv.config()
 const port = process.env.PORT ?? 3000
 
 const app = express()
@@ -14,16 +17,54 @@ const io = new Server(server, {
   }
 })
 
-io.on('connection', (socket) => {
+const db = createClient({
+  url: 'libsql://guided-hawk-ivansteb.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL
+  )
+`)
+
+io.on('connection', async (socket) => {
   console.log('Un usuario se ha conectado')
 
   socket.on('disconnect', () => {
     console.log('Un usuario se ha desconectado')
   })
 
-  socket.on('Chat message', (msg) => {
-    io.emit('Chat message', msg) // Emite el mensaje a todos los usuarios
+  socket.on('Chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit('Chat message', msg, result.lastInsertRowid.toString()) // Emite el mensaje a todos los usuarios
   })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('Chat message', row.content, row.id.toString())
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger('dev'))
