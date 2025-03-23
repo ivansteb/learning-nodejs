58dbe1a8d56862c1a1609d6615a154076da8a061
ivan <ivan.stebler@diveria.com>
2025-03-23 01:07:27 -0300

    Add username handling to chat messages and update database schema

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -25,7 +25,8 @@ const db = createClient({
 await db.execute(`
   CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT NOT NULL
+    content TEXT NOT NULL,
+    user TEXT
   )
 `)
 
@@ -38,28 +39,30 @@ io.on('connection', async (socket) => {
 
   socket.on('Chat message', async (msg) => {
     let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+
     try {
       result = await db.execute({
-        sql: 'INSERT INTO messages (content) VALUES (:msg)',
-        args: { msg }
+        sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+        args: { msg, username }
       })
     } catch (e) {
       console.error(e)
       return
     }
 
-    io.emit('Chat message', msg, result.lastInsertRowid.toString()) // Emite el mensaje a todos los usuarios
+    io.emit('Chat message', msg, result.lastInsertRowid.toString(), username) // Emite el mensaje a todos los usuarios
   })
 
   if (!socket.recovered) {
     try {
       const results = await db.execute({
-        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
         args: [socket.handshake.auth.serverOffset ?? 0]
       })
 
       results.rows.forEach(row => {
-        socket.emit('Chat message', row.content, row.id.toString())
+        socket.emit('Chat message', row.content, row.id.toString(), row.user)
       })
     } catch (e) {
       console.error(e)
